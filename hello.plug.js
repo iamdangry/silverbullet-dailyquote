a854a7799799276861eba1dea7ae31276dad4ce8
iamdangry <46516977+iamdangry@users.noreply.github.com>
2024-07-20 15:16:01 +1000

    Added Test Build

--- hello.plug.js
+++ hello.plug.js
@@ -1,1 +1,370 @@
-var x=Object.defineProperty;var P=(e,r)=>{for(var o in r)x(e,o,{get:r[o],enumerable:!0})};var p=typeof window>"u"&&typeof globalThis.WebSocketPair>"u";typeof Deno>"u"&&(self.Deno={args:[],build:{arch:"x86_64"},env:{get(){}}});var l=new Map,u=0;function a(e){self.postMessage(e)}p&&(globalThis.syscall=async(e,...r)=>await new Promise((o,n)=>{u++,l.set(u,{resolve:o,reject:n}),a({type:"sys",id:u,name:e,args:r})}));function d(e,r){p&&(self.addEventListener("message",o=>{(async()=>{let n=o.data;switch(n.type){case"inv":{let i=e[n.name];if(!i)throw new Error(`Function not loaded: ${n.name}`);try{let s=await Promise.resolve(i(...n.args||[]));a({type:"invr",id:n.id,result:s})}catch(s){console.error("An exception was thrown as a result of invoking function",n.name,"error:",s.message),a({type:"invr",id:n.id,error:s.message})}}break;case"sysr":{let i=n.id,s=l.get(i);if(!s)throw Error("Invalid request id");l.delete(i),n.error?s.reject(new Error(n.error)):s.resolve(n.result)}break}})().catch(console.error)}),a({type:"manifest",manifest:r}))}function h(e){let r=atob(e),o=r.length,n=new Uint8Array(o);for(let i=0;i<o;i++)n[i]=r.charCodeAt(i);return n}function m(e){typeof e=="string"&&(e=new TextEncoder().encode(e));let r="",o=e.byteLength;for(let n=0;n<o;n++)r+=String.fromCharCode(e[n]);return btoa(r)}async function b(e,r){if(typeof e!="string"){let o=new Uint8Array(await e.arrayBuffer()),n=o.length>0?m(o):void 0;r={method:e.method,headers:Object.fromEntries(e.headers.entries()),base64Body:n},e=e.url}return syscall("sandboxFetch.fetch",e,r)}globalThis.nativeFetch=globalThis.fetch;function v(){globalThis.fetch=async function(e,r){let o=r&&r.body?m(new Uint8Array(await new Response(r.body).arrayBuffer())):void 0,n=await b(e,r&&{method:r.method,headers:r.headers,base64Body:o});return new Response(n.base64Body?h(n.base64Body):null,{status:n.status,headers:n.headers})}}p&&v();var c={};P(c,{confirm:()=>J,copyToClipboard:()=>ce,dispatch:()=>_,downloadFile:()=>q,filterBox:()=>V,flashNotification:()=>Q,fold:()=>ee,foldAll:()=>ne,getCurrentPage:()=>A,getCursor:()=>C,getSelection:()=>F,getText:()=>S,getUiOption:()=>X,goHistory:()=>L,hidePanel:()=>N,insertAtCursor:()=>Y,insertAtPos:()=>H,moveCursor:()=>$,navigate:()=>T,openCommandPalette:()=>k,openPageNavigator:()=>E,openSearchPanel:()=>ae,openUrl:()=>B,prompt:()=>j,redo:()=>se,reloadPage:()=>R,reloadSettingsAndCommands:()=>W,reloadUI:()=>D,replaceRange:()=>I,save:()=>K,setPage:()=>w,setSelection:()=>M,setText:()=>U,setUiOption:()=>Z,showPanel:()=>G,toggleFold:()=>re,undo:()=>ie,unfold:()=>te,unfoldAll:()=>oe,uploadFile:()=>O,vimEx:()=>z});typeof self>"u"&&(self={syscall:()=>{throw new Error("Not implemented here")}});var t=globalThis.syscall;function A(){return t("editor.getCurrentPage")}function w(e){return t("editor.setPage",e)}function S(){return t("editor.getText")}function U(e){return t("editor.setText",e)}function C(){return t("editor.getCursor")}function F(){return t("editor.getSelection")}function M(e,r){return t("editor.setSelection",e,r)}function K(){return t("editor.save")}function T(e,r=!1,o=!1){return t("editor.navigate",e,r,o)}function E(e="page"){return t("editor.openPageNavigator",e)}function k(){return t("editor.openCommandPalette")}function R(){return t("editor.reloadPage")}function D(){return t("editor.reloadUI")}function W(){return t("editor.reloadSettingsAndCommands")}function B(e,r=!1){return t("editor.openUrl",e,r)}function L(e){return t("editor.goHistory",e)}function q(e,r){return t("editor.downloadFile",e,r)}function O(e,r){return t("editor.uploadFile",e,r)}function Q(e,r="info"){return t("editor.flashNotification",e,r)}function V(e,r,o="",n=""){return t("editor.filterBox",e,r,o,n)}function G(e,r,o,n=""){return t("editor.showPanel",e,r,o,n)}function N(e){return t("editor.hidePanel",e)}function H(e,r){return t("editor.insertAtPos",e,r)}function I(e,r,o){return t("editor.replaceRange",e,r,o)}function $(e,r=!1){return t("editor.moveCursor",e,r)}function Y(e){return t("editor.insertAtCursor",e)}function _(e){return t("editor.dispatch",e)}function j(e,r=""){return t("editor.prompt",e,r)}function J(e){return t("editor.confirm",e)}function X(e){return t("editor.getUiOption",e)}function Z(e,r){return t("editor.setUiOption",e,r)}function z(e){return t("editor.vimEx",e)}function ee(){return t("editor.fold")}function te(){return t("editor.unfold")}function re(){return t("editor.toggleFold")}function ne(){return t("editor.foldAll")}function oe(){return t("editor.unfoldAll")}function ie(){return t("editor.undo")}function se(){return t("editor.redo")}function ae(){return t("editor.openSearchPanel")}function ce(e){return t("editor.copyToClipboard",e)}async function f(){await c.flashNotification("Hello world!")}var g={helloWorld:f},y={name:"hello",functions:{helloWorld:{path:"hello.ts:helloWorld",command:{name:"Say hello"}}},assets:{}},Ze={manifest:y,functionMapping:g};d(g,y);export{Ze as plug};
+var __defProp = Object.defineProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+
+// https://deno.land/x/silverbullet@0.8.2/lib/plugos/worker_runtime.ts
+var runningAsWebWorker = typeof window === "undefined" && // @ts-ignore: globalThis
+typeof globalThis.WebSocketPair === "undefined";
+if (typeof Deno === "undefined") {
+  self.Deno = {
+    args: [],
+    // @ts-ignore: Deno hack
+    build: {
+      arch: "x86_64"
+    },
+    env: {
+      // @ts-ignore: Deno hack
+      get() {
+      }
+    }
+  };
+}
+var pendingRequests = /* @__PURE__ */ new Map();
+var syscallReqId = 0;
+function workerPostMessage(msg) {
+  self.postMessage(msg);
+}
+if (runningAsWebWorker) {
+  globalThis.syscall = async (name, ...args) => {
+    return await new Promise((resolve, reject) => {
+      syscallReqId++;
+      pendingRequests.set(syscallReqId, { resolve, reject });
+      workerPostMessage({
+        type: "sys",
+        id: syscallReqId,
+        name,
+        args
+      });
+    });
+  };
+}
+function setupMessageListener(functionMapping2, manifest2) {
+  if (!runningAsWebWorker) {
+    return;
+  }
+  self.addEventListener("message", (event) => {
+    (async () => {
+      const data = event.data;
+      switch (data.type) {
+        case "inv":
+          {
+            const fn = functionMapping2[data.name];
+            if (!fn) {
+              throw new Error(`Function not loaded: ${data.name}`);
+            }
+            try {
+              const result = await Promise.resolve(fn(...data.args || []));
+              workerPostMessage({
+                type: "invr",
+                id: data.id,
+                result
+              });
+            } catch (e) {
+              console.error(
+                "An exception was thrown as a result of invoking function",
+                data.name,
+                "error:",
+                e.message
+              );
+              workerPostMessage({
+                type: "invr",
+                id: data.id,
+                error: e.message
+              });
+            }
+          }
+          break;
+        case "sysr":
+          {
+            const syscallId = data.id;
+            const lookup = pendingRequests.get(syscallId);
+            if (!lookup) {
+              throw Error("Invalid request id");
+            }
+            pendingRequests.delete(syscallId);
+            if (data.error) {
+              lookup.reject(new Error(data.error));
+            } else {
+              lookup.resolve(data.result);
+            }
+          }
+          break;
+      }
+    })().catch(console.error);
+  });
+  workerPostMessage({
+    type: "manifest",
+    manifest: manifest2
+  });
+}
+function base64Decode(s) {
+  const binString = atob(s);
+  const len = binString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binString.charCodeAt(i);
+  }
+  return bytes;
+}
+function base64Encode(buffer) {
+  if (typeof buffer === "string") {
+    buffer = new TextEncoder().encode(buffer);
+  }
+  let binary = "";
+  const len = buffer.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(buffer[i]);
+  }
+  return btoa(binary);
+}
+async function sandboxFetch(reqInfo, options) {
+  if (typeof reqInfo !== "string") {
+    const body = new Uint8Array(await reqInfo.arrayBuffer());
+    const encodedBody = body.length > 0 ? base64Encode(body) : void 0;
+    options = {
+      method: reqInfo.method,
+      headers: Object.fromEntries(reqInfo.headers.entries()),
+      base64Body: encodedBody
+    };
+    reqInfo = reqInfo.url;
+  }
+  return syscall("sandboxFetch.fetch", reqInfo, options);
+}
+globalThis.nativeFetch = globalThis.fetch;
+function monkeyPatchFetch() {
+  globalThis.fetch = async function(reqInfo, init) {
+    const encodedBody = init && init.body ? base64Encode(
+      new Uint8Array(await new Response(init.body).arrayBuffer())
+    ) : void 0;
+    const r = await sandboxFetch(
+      reqInfo,
+      init && {
+        method: init.method,
+        headers: init.headers,
+        base64Body: encodedBody
+      }
+    );
+    return new Response(r.base64Body ? base64Decode(r.base64Body) : null, {
+      status: r.status,
+      headers: r.headers
+    });
+  };
+}
+if (runningAsWebWorker) {
+  monkeyPatchFetch();
+}
+
+// https://deno.land/x/silverbullet@0.8.1/plug-api/syscalls/editor.ts
+var editor_exports = {};
+__export(editor_exports, {
+  confirm: () => confirm,
+  copyToClipboard: () => copyToClipboard,
+  deleteLine: () => deleteLine,
+  dispatch: () => dispatch,
+  downloadFile: () => downloadFile,
+  filterBox: () => filterBox,
+  flashNotification: () => flashNotification,
+  fold: () => fold,
+  foldAll: () => foldAll,
+  getCurrentPage: () => getCurrentPage,
+  getCursor: () => getCursor,
+  getSelection: () => getSelection,
+  getText: () => getText,
+  getUiOption: () => getUiOption,
+  goHistory: () => goHistory,
+  hidePanel: () => hidePanel,
+  insertAtCursor: () => insertAtCursor,
+  insertAtPos: () => insertAtPos,
+  moveCursor: () => moveCursor,
+  navigate: () => navigate,
+  openCommandPalette: () => openCommandPalette,
+  openPageNavigator: () => openPageNavigator,
+  openSearchPanel: () => openSearchPanel,
+  openUrl: () => openUrl,
+  prompt: () => prompt,
+  redo: () => redo,
+  reloadPage: () => reloadPage,
+  reloadSettingsAndCommands: () => reloadSettingsAndCommands,
+  reloadUI: () => reloadUI,
+  replaceRange: () => replaceRange,
+  save: () => save,
+  setPage: () => setPage,
+  setSelection: () => setSelection,
+  setText: () => setText,
+  setUiOption: () => setUiOption,
+  showPanel: () => showPanel,
+  toggleFold: () => toggleFold,
+  undo: () => undo,
+  unfold: () => unfold,
+  unfoldAll: () => unfoldAll,
+  uploadFile: () => uploadFile,
+  vimEx: () => vimEx
+});
+
+// https://deno.land/x/silverbullet@0.8.1/plug-api/syscall.ts
+if (typeof self === "undefined") {
+  self = {
+    syscall: () => {
+      throw new Error("Not implemented here");
+    }
+  };
+}
+var syscall2 = globalThis.syscall;
+
+// https://deno.land/x/silverbullet@0.8.1/plug-api/syscalls/editor.ts
+function getCurrentPage() {
+  return syscall2("editor.getCurrentPage");
+}
+function setPage(newName) {
+  return syscall2("editor.setPage", newName);
+}
+function getText() {
+  return syscall2("editor.getText");
+}
+function setText(newText) {
+  return syscall2("editor.setText", newText);
+}
+function getCursor() {
+  return syscall2("editor.getCursor");
+}
+function getSelection() {
+  return syscall2("editor.getSelection");
+}
+function setSelection(from, to) {
+  return syscall2("editor.setSelection", from, to);
+}
+function save() {
+  return syscall2("editor.save");
+}
+function navigate(pageRef, replaceState = false, newWindow = false) {
+  return syscall2("editor.navigate", pageRef, replaceState, newWindow);
+}
+function openPageNavigator(mode = "page") {
+  return syscall2("editor.openPageNavigator", mode);
+}
+function openCommandPalette() {
+  return syscall2("editor.openCommandPalette");
+}
+function reloadPage() {
+  return syscall2("editor.reloadPage");
+}
+function reloadUI() {
+  return syscall2("editor.reloadUI");
+}
+function reloadSettingsAndCommands() {
+  return syscall2("editor.reloadSettingsAndCommands");
+}
+function openUrl(url, existingWindow = false) {
+  return syscall2("editor.openUrl", url, existingWindow);
+}
+function goHistory(delta) {
+  return syscall2("editor.goHistory", delta);
+}
+function downloadFile(filename, dataUrl) {
+  return syscall2("editor.downloadFile", filename, dataUrl);
+}
+function uploadFile(accept, capture) {
+  return syscall2("editor.uploadFile", accept, capture);
+}
+function flashNotification(message, type = "info") {
+  return syscall2("editor.flashNotification", message, type);
+}
+function filterBox(label, options, helpText = "", placeHolder = "") {
+  return syscall2("editor.filterBox", label, options, helpText, placeHolder);
+}
+function showPanel(id, mode, html, script = "") {
+  return syscall2("editor.showPanel", id, mode, html, script);
+}
+function hidePanel(id) {
+  return syscall2("editor.hidePanel", id);
+}
+function insertAtPos(text, pos) {
+  return syscall2("editor.insertAtPos", text, pos);
+}
+function replaceRange(from, to, text) {
+  return syscall2("editor.replaceRange", from, to, text);
+}
+function moveCursor(pos, center = false) {
+  return syscall2("editor.moveCursor", pos, center);
+}
+function insertAtCursor(text) {
+  return syscall2("editor.insertAtCursor", text);
+}
+function dispatch(change) {
+  return syscall2("editor.dispatch", change);
+}
+function prompt(message, defaultValue = "") {
+  return syscall2("editor.prompt", message, defaultValue);
+}
+function confirm(message) {
+  return syscall2("editor.confirm", message);
+}
+function getUiOption(key) {
+  return syscall2("editor.getUiOption", key);
+}
+function setUiOption(key, value) {
+  return syscall2("editor.setUiOption", key, value);
+}
+function vimEx(exCommand) {
+  return syscall2("editor.vimEx", exCommand);
+}
+function fold() {
+  return syscall2("editor.fold");
+}
+function unfold() {
+  return syscall2("editor.unfold");
+}
+function toggleFold() {
+  return syscall2("editor.toggleFold");
+}
+function foldAll() {
+  return syscall2("editor.foldAll");
+}
+function unfoldAll() {
+  return syscall2("editor.unfoldAll");
+}
+function undo() {
+  return syscall2("editor.undo");
+}
+function redo() {
+  return syscall2("editor.redo");
+}
+function openSearchPanel() {
+  return syscall2("editor.openSearchPanel");
+}
+function copyToClipboard(data) {
+  return syscall2("editor.copyToClipboard", data);
+}
+function deleteLine() {
+  return syscall2("editor.deleteLine");
+}
+
+// E:/Repos/silverbullet-dailyquote/hello.ts
+async function helloWorld() {
+  await editor_exports.flashNotification("Hello world!");
+}
+
+// 5422d1cca8b37403.js
+var functionMapping = {
+  helloWorld
+};
+var manifest = {
+  "name": "hello",
+  "functions": {
+    "helloWorld": {
+      "path": "hello.ts:helloWorld",
+      "command": {
+        "name": "Say hello"
+      }
+    }
+  },
+  "assets": {}
+};
+var plug = { manifest, functionMapping };
+setupMessageListener(functionMapping, manifest);
+export {
+  plug
+};
+//# sourceMappingURL=hello.plug.js.map
